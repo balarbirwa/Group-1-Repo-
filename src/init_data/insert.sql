a0ef79217c1eb3e2e3852c6d5a40d9135c3b6aa1
beaucave <moanacave@gmail.com>
2022-11-10 14:29:01 -0700

    updated insert.sql; projects ejs templates -B

--- src/init_data/insert.sql
+++ src/init_data/insert.sql
@@ -1,8 +1,22 @@
 INSERT INTO users (
-    username,
-    firstName, 
-    lastName, 
-    isManager, 
-    password) VALUES
-('CEOMan', 'Richard', 'Mann', TRUE, '$2y$10$T9qBP0w5uzEMdYhznkklje9U64X/NhHUjx2SAga0VBt1ENUuS2Ffm')
+        username,
+        firstName,
+        lastName,
+        isManager,
+        password
+    )
+VALUES (
+        'Employee1',
+        'Firstname',
+        'Lastname',
+        TRUE,
+        'password'
+    ),
+    (
+        'CEOMan',
+        'Richard',
+        'Mann',
+        TRUE,
+        '$2y$10$T9qBP0w5uzEMdYhznkklje9U64X/NhHUjx2SAga0VBt1ENUuS2Ffm'
+    ),
 ;

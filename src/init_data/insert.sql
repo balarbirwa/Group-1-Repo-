1e51f2f346280bde1eea6f272e79e0c4cb9ffa52
Joshua Dinerman <45548505+JoshuaD9907@users.noreply.github.com>
2022-11-10 13:17:33 -0700

    Added test user to database

---
+++ src/init_data/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (
+    username,
+    firstName, 
+    lastName, 
+    isManager, 
+    password) VALUES
+('CEOMan', 'Richard', 'Mann', TRUE, '$2y$10$T9qBP0w5uzEMdYhznkklje9U64X/NhHUjx2SAga0VBt1ENUuS2Ffm')
+;
+

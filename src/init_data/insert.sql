01c0de9868794bd10651fef6933b0f607d5b780f
beaucave <moanacave@gmail.com>
2022-11-10 14:26:36 -0700

    fixing insert merge error? -B

--- src/init_data/insert.sql
+++ src/init_data/insert.sql
@@ -4,6 +4,5 @@ INSERT INTO users (
     lastName, 
     isManager, 
     password) VALUES
-('Employee1', 'Firstname', 'Lastname', TRUE, 'password')
+('CEOMan', 'Richard', 'Mann', TRUE, '$2y$10$T9qBP0w5uzEMdYhznkklje9U64X/NhHUjx2SAga0VBt1ENUuS2Ffm')
 ;
-

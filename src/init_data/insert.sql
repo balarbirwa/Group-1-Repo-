187d6687fc21e41f4f1b7c98faafb878d70b920a
beaucave <moanacave@gmail.com>
2022-11-11 13:37:33 -0700

    constructing empty projects database -B

--- src/init_data/insert.sql
+++ src/init_data/insert.sql
@@ -1,21 +1,19 @@
-        username,
-        firstName,
-        lastName,
-        isManager,
-        password
-    )
-VALUES (
-        'Employee1',
-        'Firstname',
-        'Lastname',
-        TRUE,
-        'password'
-    ),
-    (
-        'CEOMan',
-        'Richard',
-        'Mann',
-        TRUE,
-        '$2y$10$T9qBP0w5uzEMdYhznkklje9U64X/NhHUjx2SAga0VBt1ENUuS2Ffm'
-    ),
+INSERT INTO users (
+    username,
+    firstName, 
+    lastName, 
+    isManager, 
+    password) VALUES
+('CEOMan', 'Richard', 'Mann', TRUE, '$2y$10$T9qBP0w5uzEMdYhznkklje9U64X/NhHUjx2SAga0VBt1ENUuS2Ffm'),
+('Employee1', 'Firstname', 'Lastname', TRUE, 'password')
+;
+
+INSERT INTO projects (
+    projectName,
+    description, 
+    lastName, 
+    isManager, 
+    password) VALUES
+('Project 1', 'The first project'),
+('Project 2', 'The second project')
 ;

cc2ebb70a7ef2907d3c8615a303dde54adb566d6
leonorastiernborg <leonora.stiernborg@gmail.com>
2022-11-12 15:29:15 -0700

    Fixed database issue with foreign keys

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY, 
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50) UNIQUE,
     firstname VARCHAR(50) NOT NULL, 
     lastname VARCHAR(50) NOT NULL, 
     ismanager BOOLEAN, 
@@ -9,13 +9,19 @@ CREATE TABLE users(
     password VARCHAR(256)
 );
 
---DROP TABLE IF EXISTS projects;
---CREATE TABLE projects(
---    project_id SERIAL PRIMARY KEY, 
---    projectName VARCHAR(50), 
---    description VARCHAR(500), 
---    FOREIGN KEY (user_id) REFERENCES users(user_id)
---);
+DROP TABLE IF EXISTS projects;
+CREATE TABLE projects(
+    project_id SERIAL PRIMARY KEY, 
+    projectName VARCHAR(50), 
+    description VARCHAR(500), 
+    user_id integer REFERENCES users(user_id)
+);
+
+DROP TABLE IF EXISTS users_to_projects;
+CREATE TABLE users_to_projects(
+    user_id integer REFERENCES users(user_id),
+    project_id integer REFERENCES projects(project_id)
+);
 
 --DROP TABLE IF EXISTS users_to_projects;
 --CREATE TABLE users_to_projects(

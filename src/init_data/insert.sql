2f0562c94097900eb064a095f3a053c9632fd178
leonorastiernborg <leonora.stiernborg@gmail.com>
2022-11-12 17:05:33 -0700

    Added fetch courses functionallity to node.js and added some mock data to database for testing

--- src/init_data/insert.sql
+++ src/init_data/insert.sql
@@ -1,3 +1,15 @@
-INSERT INTO users (username, firstname, lastname, ismanager, password) VALUES
-('CEOMan', 'Richard', 'Mann', TRUE, '$2b$10$I75WivfjbJCFZc0phyDM9Ot/awMxsqk9cE9iPyNh1ODsWolzUKWiu')
+INSERT INTO users (username, first_name, last_name, is_manager, password) VALUES
+('CEOMan', 'Richard', 'Mann', FALSE, '$2b$10$I75WivfjbJCFZc0phyDM9Ot/awMxsqk9cE9iPyNh1ODsWolzUKWiu'),
+('123', 'bob', 'bob_last_name', FALSE, '$2b$10$I75WivfjbJCFZc0phyDM9Ot/awMxsqk9cE9iPyNh1ODsWolzUKWiu')
+;
+
+INSERT INTO projects (project_name, description) VALUES
+('Course 1', 'This is a really fun course'),
+('Course 2', 'Another fun course')
+;
+
+INSERT INTO users_to_projects (user_id, project_id) VALUES
+(1, 1),
+(1, 2),
+(2, 2)
 ;
